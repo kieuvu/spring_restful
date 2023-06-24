d8c4edaa51fd3e163914dad9de97a0e796385895
“kieuvu” <kieuvuvuvu@gmail.com>
2023-06-25 01:42:09 +0900

    set default api prefix

---
+++ src/main/java/com/vukm/restapi/exceptions/api/ApiException.java
@@ -0,0 +1,26 @@
+package com.vukm.restapi.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+/**
+ * @author kieuvu
+ * Created on 25/06/2023.
+ */
+public class ApiException extends RuntimeException {
+
+    private final HttpStatus status;
+    private final Object     info;
+
+    public ApiException(HttpStatus status, Object info) {
+        this.info   = info;
+        this.status = status;
+    }
+
+    public HttpStatus getStatus() {
+        return this.status;
+    }
+
+    public Object getInfo() {
+        return this.info;
+    }
+}
