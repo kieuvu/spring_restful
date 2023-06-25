c3972be29962e853ead9e3e71951c24a92d97eec
“kieuvu” <kieuvuvuvu@gmail.com>
2023-06-26 00:34:07 +0900

    handle validate exeption

--- src/main/java/com/vukm/restapi/exceptions/api/ApiExceptionHandler.java
+++ src/main/java/com/vukm/restapi/exceptions/api/ApiExceptionHandler.java
@@ -1,9 +1,14 @@
 package com.vukm.restapi.exceptions.api;
 
+import com.vukm.restapi.utils.request.FailedRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author kieuvu
@@ -12,12 +17,28 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ApiExceptionHandler {
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error");
+    public ResponseEntity<FailedRequest> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                             .body(new FailedRequest(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage()));
     }
 
     @ExceptionHandler({ApiException.class})
-    public ResponseEntity<Object> handleApiException(ApiException ex) {
-        return ResponseEntity.status(ex.getStatus()).body(ex.getInfo());
+    public ResponseEntity<FailedRequest> handleApiException(ApiException ex) {
+        return ResponseEntity.status(ex.getStatus())
+                             .body(new FailedRequest(ex.getStatus(), ex.getInfo()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<FailedRequest> handleValidationException(MethodArgumentNotValidException ex) {
+        List<String> errorMessages = new ArrayList<>();
+
+        ex.getBindingResult()
+          .getAllErrors()
+          .forEach(error -> {
+              errorMessages.add(error.getDefaultMessage());
+          });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                             .body(new FailedRequest(HttpStatus.BAD_REQUEST, errorMessages));
     }
 }
