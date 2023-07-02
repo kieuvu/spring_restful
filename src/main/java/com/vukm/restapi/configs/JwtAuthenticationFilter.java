c9d49261a1ffb98a11aed6820e1a47ca57c3e9db
“kieuvu” <kieuvuvuvu@gmail.com>
2023-07-02 22:38:10 +0700

    jwt request filter

---
+++ src/main/java/com/vukm/restapi/configs/JwtAuthenticationFilter.java
@@ -0,0 +1,62 @@
+package com.vukm.restapi.configs;
+
+import com.vukm.restapi.services.jwt.JwtService;
+import com.vukm.restapi.services.jwt.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author kieuvu
+ * Created on 02/07/2023.
+ */
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private final JwtService      jwtService;
+    @Autowired
+    private final UserInfoService userInfoService;
+
+    public JwtAuthenticationFilter(UserInfoService userInfoService, JwtService jwtService) {
+        this.userInfoService = userInfoService;
+        this.jwtService      = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        String token      = null;
+        String username   = null;
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            token    = authHeader.substring(7);
+            username = jwtService.extractUsername(token);
+        }
+
+        if (username != null && SecurityContextHolder.getContext()
+                                                     .getAuthentication() == null) {
+            UserDetails userDetails = this.userInfoService.loadUserByUsername(username);
+            if (jwtService.validateToken(token, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
+                                                                                                        null,
+                                                                                                        userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext()
+                                     .setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
